a2a07346f8eacbbf462fce1beef2836efd748a39
gregory6z <gregoryrag@gmail.com>
2022-08-23 22:00:24 +0200

    Add trasactions Context ✔️

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+interface ITransaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface TransactionsContextType {
+  transactions: ITransaction[];
+}
+
+export const TransactionsContext = createContext({} as TransactionsContextType);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
